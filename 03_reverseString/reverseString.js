56eb80669b832e4e588c5df1d69e2c85b05969cc
tvoisyabr <neperamojny@gmail.com>
2024-07-01 10:25:09 -0700

    Add solution for 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strArray = str.split("");
+    let strArrayReversed = strArray.reverse();
+    let strReversed = strArrayReversed.join("");
 
+    return strReversed;
 };
 
 // Do not edit below this line
