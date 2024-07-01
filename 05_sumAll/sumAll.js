7ef2431702ef6768b81ec058823aba6cb0eedc39
tvoisyabr <neperamojny@gmail.com>
2024-07-01 12:15:18 -0700

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    let sum = 0;
 
+    if(firstNum < 0 || secondNum < 0 || typeof firstNum !== "number" || typeof secondNum !== "number") {
+        sum = "ERROR";
+    } else 
+    if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        } 
+    } else if (secondNum > firstNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        } 
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
