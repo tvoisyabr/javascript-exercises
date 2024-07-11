d805d0949bcf6aeb9f12daf06046c3f605b83a17
tvoisyabr <neperamojny@gmail.com>
2024-07-11 12:45:52 -0700

    Add solution for 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let normStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let reversedStr = normStr.split('').reverse().join('');
+  return normStr === reversedStr;
 };
 
 // Do not edit below this line
