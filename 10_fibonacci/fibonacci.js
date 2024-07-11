06a908f1f81fff5eeaf1afe82badc55713a8295b
tvoisyabr <neperamojny@gmail.com>
2024-07-11 13:16:35 -0700

    Add solution for 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    index = Number(index);
+
+    if(index === 0) return 0;
+    if(index < 0) return 'OOPS';
+    
+    const sequence = [1, 1];
+
+    for (let i = 2; i < index; i++) {
+        sequence.push(sequence[i -1] + sequence[i -2]);
+    }
+
+    return sequence[index - 1];
 };
 
 // Do not edit below this line
